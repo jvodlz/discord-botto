92afff4a43e1aabe89b50c272397c4197ecf5737
JT <95458994+jvodlz@users.noreply.github.com>
2025-03-04 18:31:59 +1300

    update initial responses

--- main.py
+++ main.py
@@ -13,34 +13,63 @@ intents = discord.Intents.default()
 intents.message_content = True
 client = discord.Client(intents=intents)
 
-grey_words = ['unhappy', 'sorrowful', 'dejected', 'regretful', 'depressed', 'downcast', 'miserable', 'downhearted', 'down', 'despondent', 'despairing', 'disconsolate', 'out of sorts', 'desolate', 'bleak', 'wretched', 'glum', 'gloomy', 'doleful', 'dismal', 'blue', 'melancholy', 'melancholic', 'low-spirited', 'mournful', 'woeful', 'forlorn', 'crestfallen', 'broken-hearted', 'heartbroken', 'inconsolable', 'grief-stricken', 'down in the dumps', 'tragic', 'unfortunate', 'feel awful', 'pitiful', 'pitiable', 'grievous', 'traumatic', 'upsetting', 'depressing', 'distressing', 'dispiriting', 'heartbreaking', 'agonizing', 'distressed', 'distressful', 'regrettable', 'feel sorry', 'deplorable', 'lamentable', 'pathetic', 'shameful', 'disgraceful', 'sad', 'saddened', 'dispirited', 'low', 'heavy-hearted', 'morose', 'weighed down', 'oppressed', 'tearful', 'upset', 'disheartened', 'discouraged', 'daunted', 'pessimistic', 'fed up', 'feel shit']
 
-starter_encouragements = [
-  "Hang in there",
-  "You'll get through this",
-  "Just remember, peaks and troughs"
-]
+"""
+Initial Responses
+"""
+grey_words = {
+    'unhappy', 'sorrowful', 'dejected', 'regretful', 'depressed', 'downcast',
+    'miserable', 'downhearted', 'down', 'despondent', 'despairing',
+    'disconsolate', 'out of sorts', 'desolate', 'bleak', 'wretched', 'glum',
+    'gloomy', 'doleful', 'dismal', 'blue', 'melancholy', 'melancholic',
+    'low-spirited', 'mournful', 'woeful', 'forlorn', 'crestfallen',
+    'broken-hearted', 'heartbroken', 'inconsolable', 'grief-stricken',
+    'down in the dumps', 'tragic', 'unfortunate', 'feel awful', 'pitiful',
+    'pitiable', 'grievous', 'traumatic', 'upsetting', 'depressing',
+    'distressing', 'dispiriting', 'heartbreaking', 'agonizing', 'distressed',
+    'distressful', 'regrettable', 'feel sorry', 'deplorable', 'lamentable',
+    'pathetic', 'shameful', 'disgraceful', 'sad', 'saddened', 'dispirited',
+    'low', 'heavy-hearted', 'morose', 'weighed down', 'oppressed', 'tearful',
+    'upset', 'disheartened', 'discouraged', 'daunted', 'pessimistic', 'fed up',
+    'feel shit'
+}
 
-# create separate dictionary for making edits to command
+encouragements = {
+    "Hang in there", "You'll get through this",
+    "Just remember, peaks and troughs",
+    "You can demand the best, it's not selfish to care for yourself",
+    "There there",
+    "Remember to breathe; inhale and exhale deeply",
+    "Have пряник, it's so scrummy that you'll crack a smile. Or maybe you know it as lebkuchen",
+    "Gimme a sec to collect my farts, they are funny right?"
+}
+
+# For Development ONLY
 command_help = {
-    ".inspire": "Returns a random quote", 
-    ".new_enc": "Add encouragement", 
-    ".del_enc": "Remove encouragement", 
-    ".list_enc": "Display ALL encouragements", 
-    ".respond_enc": "Toggle encouragement Message",
-    ".toggle_otto": "Instruct dog to come or go"
-    }
+    ".new_enc": "Ajouter un encouragement",
+    ".del_enc": "Remove encouragement",
+    ".list_enc": "Display **ALL** encouragements",
+    ".new_greet": 'Add new greeting. English|How can I help?,Sup,Woof!,Hi there',
+    ".del_greet_lang": "Remove language greeting",
+    ".list_greet": "Display **ALL** greetings"
+}
 
-# TO-DO: Write Fn to Add/Del otto_talk,  db["default_greeting_key"]
 otto_talk = {
-    "greet" : {
-        "English" : ["How can I help?", "Sup", "Woof!", "Hi there"], 
-        "French" : ["Comment puis-je aider ?", "Quoi de neuf ?", "Wouf !", "Coucou"],
-        "Spanish" : ["¿ Cómo puedo ayudar ?", "¿ Qué tal ?", "¡ Jau !", "Hola"],
-        "Russian" : ["Чем я могу помочь?", "Что нового?", "Гав!", "Привет"],
-        "German" : ["Wie kann ich helfen?", "Wie geht's?", "Wuff!", "Grüß dich"],
-        "Dutch" : ["Hoe kan ik helfen?", "Hé", "Woef!", "Hallo daar"]
-    }
+    "greet": {
+        "English": ["How can I help?", "Sup", "Woof!", "Hi there"],
+        "Bosnian": ["Kako mogu pomoći?", "Šta ima?", "Av!", "Zdravo"],
+        "French": ["Comment puis-je aider ?", "Quoi de neuf ?", "Wouf !", "Coucou"],
+        "Spanish": ["¿ Cómo puedo ayudar ?", "¿ Qué tal ?", "¡ Jau !", "Hola"],
+        "Russian": ["Чем я могу помочь?", "Что нового?", "Гав!", "Привет"],
+        "German": ["Wie kann ich helfen?", "Wie geht's?", "Wuff!", "Grüß dich"],
+        "Dutch": ["Hoe kan ik helfen?", "Hé", "Woef!", "Hallo daar"]
+    },
+    "philosopher": {"epictetus", "marcus aurelius", "seneca"}
+}
+
+toggles = {
+    "respond_enc": True,
+    "otto": True
 }
 
 if "respond_enc" not in db.keys():
