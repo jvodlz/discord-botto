dc254b09076474ac012312b363d8add99e94f265
JT <95458994+jvodlz@users.noreply.github.com>
2025-03-21 20:50:08 +1300

    feat: add default command

--- src/app/main.py
+++ src/app/main.py
@@ -5,7 +5,42 @@ from discord_interactions import verify_key_decorator
 from asgiref.wsgi import WsgiToAsgi
 from mangum import Mangum
 
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-from config.config import PUBLIC_KEY
+from config import PUBLIC_KEY
+from commands import greet
 
+app = Flask(__name__)
+asgi_app = WsgiToAsgi(app)
+handler = Mangum(asgi_app)
 
+@app.route("/", methods=["POST"])
+async def interactions():
+    # Debug logging
+    raw_request = request.json
+    print(f"Request: {raw_request}")
+    return interact(raw_request)
+
+# @verify_key_decorator(PUBLIC_KEY)
+def interact(raw_request):
+    response_data = {"type": 1}
+    if raw_request["type"] == 1: # PING
+        return jsonify(response_data)
+    elif raw_request["type"] == 2: # APPLICATION_COMMAND
+        data = raw_request["data"]
+        command_name = data["name"]
+
+        if command_name == "hello":
+            message_content = greet()
+
+        # Interaction Callback Type 4 : Respond to interaction with message 
+        response_data = {
+            "type": 4,
+            "data": {
+                "content": message_content,
+            }
+        }
+
+        return jsonify(response_data)
+    
+
+if __name__ == "__main__":
+    app.run(debug=True)
