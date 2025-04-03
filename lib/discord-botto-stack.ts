226f0df39ae94522621f0c93dc812f0c854e4695
JT <95458994+jvodlz@users.noreply.github.com>
2025-04-04 12:32:59 +1300

    deploy: implement Docker image deployment to AWS

--- lib/discord-botto-stack.ts
+++ lib/discord-botto-stack.ts
@@ -1,16 +1,44 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as lambda from "aws-cdk-lib/aws-lambda"
+import * as ssm from "aws-cdk-lib/aws-ssm"
 
 export class DiscordBottoStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const discordPublicKey = ssm.StringParameter.fromStringParameterAttributes(this, 
+      "PublicKey", {
+        parameterName: "/discord/botto/public/key",
+      }
+    );
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'DiscordBottoQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const dockerFunction = new lambda.DockerImageFunction(
+      this, "DockerFunction", {
+        code: lambda.DockerImageCode.fromImageAsset("./src"),
+        memorySize: 1024,
+        timeout: cdk.Duration.seconds(10),
+        environment: {
+          PUBLIC_KEY: discordPublicKey.stringValue
+        },
+    });
+
+    const functionUrl = dockerFunction.addFunctionUrl({
+      authType: lambda.FunctionUrlAuthType.NONE,
+      cors: {
+        allowedOrigins: ["*"],
+        allowedMethods: [
+          lambda.HttpMethod.GET,
+          lambda.HttpMethod.POST,
+          lambda.HttpMethod.PATCH
+        ],
+        allowedHeaders: ["*"]
+      }
+    });
+
+    new cdk.CfnOutput(this, "FunctionUrl", {
+      value: functionUrl.url
+    });
+
   }
 }
